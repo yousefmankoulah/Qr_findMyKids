da1ff8e610a6251619e2cea2d3749ff45be408ef
yousefmankoulah <45926659+yousefmankoulah@users.noreply.github.com>
2023-09-11 15:05:25 +0000

    s

--- findMe/home/views.py
+++ findMe/home/views.py
@@ -1,3 +1,46 @@
-from django.shortcuts import render
+from django import urls
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import login, logout, authenticate
+from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required
+from django.conf import settings
+#from qrcode import *
+import time
+from django.contrib import messages
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+
 
 # Create your views here.
+
+def home(request):
+    return render(request, 'home.html')
+
+
+def signInView(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            username = request.POST['username']
+            password = request.POST['password']
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                messages.info(request, "You are now logged in.")
+                return redirect('home')
+            else:
+                messages.error(request,"Invalid username or password.")
+
+        else:
+            messages.error(request,"Invalid username or password.")
+
+    else:
+        form = AuthenticationForm()
+    return render(request, 'login.html', {'form': form})
+
+
+
+
+def signoutView(request):
+    logout(request)
+    return redirect('login')
