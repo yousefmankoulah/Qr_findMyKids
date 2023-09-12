db80ed214eba1eb3e39244c094812850885956af
yousefmankoulah <45926659+yousefmankoulah@users.noreply.github.com>
2023-09-12 16:50:29 +0000

    u

--- findMe/order/urls.py
+++ findMe/order/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
+from . import views
 
 
 urlpatterns = [
-
+    path('thanks/<int:order_id>/', views.thanks, name='thanks'),
+    path('history/', views.orderHistory, name='order_history'),
+    path('<int:order_id>/', views.viewOrder, name='order_detail'),
 ]
