216aec4303328e1828a6879e4b2bd158a643d6a4
yousefmankoulah <45926659+yousefmankoulah@users.noreply.github.com>
2023-09-14 20:10:30 +0000

    u

--- findMe/order/views.py
+++ findMe/order/views.py
@@ -10,6 +10,7 @@ def thanks(request, order_id):
         customer_order = get_object_or_404(Order, id=order_id)
     return render(request, 'thanks.html', {'customer_order': customer_order})
 
+
 @login_required()
 def orderHistory(request):
     if request.user.is_authenticated:
