649cf0504c725a516f91d65da31aab6952ed3daa
yousefmankoulah <45926659+yousefmankoulah@users.noreply.github.com>
2023-09-16 11:38:59 +0000

    u

--- findMe/home/forms.py
+++ findMe/home/forms.py
@@ -1,11 +1,12 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from django.utils.translation import gettext_lazy as _
 
 
 class SignUpForm(UserCreationForm):
-    email = forms.EmailField(max_length=150)
+    email = forms.EmailField(max_length=150, label=_("Email"))
 
     class Meta:
         model = User
-        fields = ('first_name', 'last_name', 'email', 'username', 'password1', 'password2')
+        fields = (_('first_name'), 'last_name', 'email', 'username', 'password1', 'password2')
