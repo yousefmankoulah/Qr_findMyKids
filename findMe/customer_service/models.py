f42002628d81431f7b7731e6f5f0c771f9dcfa56
mankoulah <111663447+mankoulah@users.noreply.github.com>
2023-09-21 17:36:55 +0000

    u

--- findMe/customer_service/models.py
+++ findMe/customer_service/models.py
@@ -15,6 +15,15 @@ class Customer_service(models.Model):
     order_number = models.CharField(max_length=300,blank=True, null=True)
     category = models.CharField(max_length=200, blank=True, null=True)
 
+    def save(self, *args, **kwargs):
+        self.customer_name = self.customer_name.lower()
+        self.customer_service_rep = self.customer_service_rep.lower()
+        self.customer_email = self.customer_email.lower()
+        self.customer_title = self.customer_title.lower()
+        self.customer_description = self.customer_description.lower()
+        self.category = self.category.lower()
+        return super(Customer_service, self).save(*args, **kwargs)
+
     class Meta:
         db_table = 'customer_service'
         verbose_name = 'Customer Service'
