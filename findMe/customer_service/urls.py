d2c3e4692949c668a2e5d135f8f41d18f6c17c9e
mankoulah <111663447+mankoulah@users.noreply.github.com>
2023-09-21 01:03:05 +0000

    u

--- findMe/customer_service/urls.py
+++ findMe/customer_service/urls.py
@@ -3,5 +3,9 @@ from . import views
 
 urlpatterns = [
     path('', views.contactus, name='contactus'),
+    path('opendTickets/', views.opendTickets, name='opendTickets'),
+    path('allTickets', views.allTickets, name='allTickets'),
+    path('ticketDetails/<int:id>', views.updateTickets, name='updateTickets'),
+    path('closedTickets/', views.closedTickets, name="closedTickets"),
 ]
     
--- findMe/customer_service/views.py
+++ findMe/customer_service/views.py
@@ -4,6 +4,8 @@ from django.template.loader import render_to_string
 from django.utils.html import strip_tags
 from django.core.mail import send_mail
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+
 
 # Create your views here.
 
@@ -39,3 +41,48 @@ def contactus(request):
         return redirect('category')
         
     return render(request, 'contactus.html')
+
+@login_required('login')
+def updateTickets(request, id):
+    customer_service = Customer_service.objects.filter(id=id)
+    if request.user.is_superuser:
+        if request.method == 'POST':
+            customer_name = request.POST['customer_name']
+            customer_phone = request.POST['customer_phone']
+            customer_email = request.POST['customer_email']
+            category = request.POST['category']
+            customer_title = request.POST['customer_title']
+            order_number = request.POST['order_number']
+            customer_service_rep = request.POST['customer_service_rep']
+            ticket_status = request.POST.get('ticket_status')
+            customer_description = request.POST.get('customer_description')
+
+            customer_service.customer_name = customer_name
+            customer_service.customer_phone = customer_phone
+            customer_service.customer_email = customer_email
+            customer_service.category = category
+            customer_service.customer_title = customer_title
+            customer_service.order_number = order_number
+            customer_service.customer_service_rep = customer_service_rep
+            customer_service.customer_description = customer_description
+            customer_service.ticket_status = ticket_status == 'on'
+            customer_service.save()
+            return redirect('opendTickets')
+    return render(request, 'ticketDetail.html', {'customer_service': customer_service})
+
+@login_required('login')
+def opendTickets(request):
+    customer_service = Customer_service.objects.filter(ticket_status= False).order_by('id').values()
+    return render(request, 'customerserviceDashboard.html', {'customer_service': customer_service})
+
+
+@login_required('login')
+def allTickets(request):
+    customer_service = Customer_service.objects.all().order_by('id').values()
+    return render(request, 'customerserviceDashboard.html', {'customer_service': customer_service})
+
+
+@login_required('login')
+def closedTickets(request):
+    customer_service = Customer_service.objects.filter(ticket_status= True).order_by('id').values()
+    return render(request, 'customerserviceDashboard.html', {'customer_service': customer_service})
